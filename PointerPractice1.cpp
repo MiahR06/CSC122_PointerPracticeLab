a52688694387f87ddf17f018b81dd233d1945ea1
Raniya <miahraniya@gmail.com>
2025-09-07 14:16:05 -0500

    Create PointerPractice1.cpp

---
+++ PointerPractice1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+    int a, b;
+    cout << "Enter an integer: ";
+    cin >> a;
+    cout << "Enter another integer: ";
+    cin >> b;
+
+    int* ptr_a = &a;        // Pointer for a
+    int* ptr_b = &b;        // Pointer for b
+
+    cout << "\nYou entered " << *ptr_a << " and " << *ptr_b;    // Dereferences pointers to print out original variables
+
+}
